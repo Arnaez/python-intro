12872c69ba12cc61f07c2ff76bffaf528520642c
Arnaez <40775988+Arnaez@users.noreply.github.com>
2018-12-23 15:30:19 +0000

    Initial Commit: Test

--- Newton-Raphson.py
+++ Newton-Raphson.py
@@ -4,4 +4,24 @@
 epsilon = 0.01
 k = 24.0
 guess = k/2.0
-while abs(guess*guess -k) >= epsilon:
+guesses = 0
+while abs(guess*guess - k) >= epsilon:
+    guesses += 1
+    guess = guess - (((guess**2) - k)/(2*guess))
+print('Square root of', k, 'is about', guess)
+print('This took', guesses, 'guesses using Newton-Raphson.')
+
+low = min(k, 0)
+high = max(1.0, k)
+ans = (high + low)/2.0
+guesses = 0
+while abs(ans**2 - k) >= epsilon:
+    guesses += 1
+    if ans**2 > k:
+        high = ans
+    else:
+        low = ans
+    ans = (high + low)/2.0
+
+print('Square root of', k, 'is about', guess)
+print('This took', guesses, 'guesses in a bijection search.')
